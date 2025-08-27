db7ed6a94d79cdac6a46c5dad7194da97b4cebef
mbuhion <tur49580@temple.edu>
2025-08-27 18:12:12 -0400

    completed rand_string() function code

--- random.c
+++ random.c
@@ -2,13 +2,19 @@
 #include <stdlib.h>
 #include <time.h>
 
-char rand_string(char* string, size_t size){
+char* rand_string(char *string, size_t size){
     // create const char string for each letter of the alphabet
-    const char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+    if(size == 0){
+        return NULL; // if the size is 0, no letters will be assigned to string. return null
+    }
 
-        //loop through
-        for(int i = 0; i < size; ++i){
-            
-        }
-    return s;
+    //loop through index of string and assign a random char until string is of size
+    for(size_t i = 0; i < size; i++){
+        int num = rand() % (int)(sizeof(alpha) - 1); // generates num for index of random char
+        string[i] = alpha[num]; // assigns random char to string
+    }
+
+    string[size - 1] = '\0'; // null terminate the end of string
+    return string;
 }
