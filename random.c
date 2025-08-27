2939c70b6daa7eb040f80ec64a21addd6a965b9a
mbuhion <tur49580@temple.edu>
2025-08-27 14:32:30 -0400

    made notes

---
+++ random.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char rand_string(char* string, size_t size){
+    // create const char string for each letter of the alphabet
+    const char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        //loop through
+        for(int i = 0; i < size; ++i){
+            
+        }
+    return s;
+}
